a51a4825ec69b04d138006993f669ad0205a8803
KittyCathy <146510476+MZDM-B@users.noreply.github.com>
2024-11-29 16:14:26 +0800

    Add files via upload
    newer FPS Counter

--- source/debug/FPSCounter.hx
+++ source/debug/FPSCounter.hx
@@ -7,8 +7,8 @@ import openfl.system.System as OpenFlSystem;
 import lime.system.System as LimeSystem;
 
 /**
-	The FPS class provides an easy-to-use monitor to display
-	the current frame rate of an OpenFL project
+    The FPS class provides an easy-to-use monitor to display
+    the current frame rate of an OpenFL project
 **/
 #if cpp
 #if windows
@@ -21,123 +21,139 @@ import lime.system.System as LimeSystem;
 #end
 class FPSCounter extends TextField
 {
-	/**
-		The current frame rate, expressed using frames-per-second
-	**/
-	public var currentFPS(default, null):Int;
+    /**
+        The current frame rate, expressed using frames-per-second
+    **/
+    public var currentFPS(default, null):Int;
 
-	/**
-		The current memory usage (WARNING: this is NOT your total program memory usage, rather it shows the garbage collector memory)
-	**/
-	public var memoryMegas(get, never):Float;
+    /**
+        The current memory usage (WARNING: this is NOT your total program memory usage, rather it shows the garbage collector memory)
+    **/
+    public var memoryMegas(get, never):Float;
 
-	@:noCompletion private var times:Array<Float>;
+    /**
+        The peak memory usage
+    **/
+    public var memoryPeakMegas(get, never):Float;
 
-	public var os:String = '';
+    @:noCompletion private var times:Array<Float>;
+    private var memoryPeak:Float = 0;
 
-	public function new(x:Float = 10, y:Float = 10, color:Int = 0x000000)
-	{
-		super();
+    public var os:String = '';
 
-		#if !officialBuild
-		if (LimeSystem.platformName == LimeSystem.platformVersion || LimeSystem.platformVersion == null)
-			os = '\nOS: ${LimeSystem.platformName}' #if cpp + ' ${getArch() != 'Unknown' ? getArch() : ''}' #end;
-		else
-			os = '\nOS: ${LimeSystem.platformName}' #if cpp + ' ${getArch() != 'Unknown' ? getArch() : ''}' #end + ' - ${LimeSystem.platformVersion}';
-		#end
+    public function new(x:Float = 10, y:Float = 0, color:Int = 0x000000)
+    {
+        super();
 
-		positionFPS(x, y);
+        #if !officialBuild
+        if (LimeSystem.platformName == LimeSystem.platformVersion || LimeSystem.platformVersion == null)
+            os = '\n当前为测试模式\nCurrent OS: ${LimeSystem.platformName}' #if cpp + ' ${getArch() != 'Unknown' ? getArch() : ''}' #end;
+        else
+            os = '\n当前为测试模式\nCurrent OS: ${LimeSystem.platformName}' #if cpp + ' ${getArch() != 'Unknown' ? getArch() : ''}' #end + ' - ${LimeSystem.platformVersion}';
+        #end
 
-		currentFPS = 0;
-		selectable = false;
-		mouseEnabled = false;
-		defaultTextFormat = new TextFormat("_sans", 14, color);
-		width = FlxG.width;
-		multiline = true;
-		text = "FPS: ";
+        positionFPS(x, y);
 
-		times = [];
-	}
+        currentFPS = 0;
+        selectable = false;
+        mouseEnabled = false;
+        //defaultTextFormat = new TextFormat("_sans", 14, color);
+        defaultTextFormat = new TextFormat(openfl.utils.Assets.getFont("assets/fonts/cnfont.ttf").fontName, 13, color);
 
-	var deltaTimeout:Float = 0.0;
+        width = FlxG.width;
+        multiline = true;
+        text = "Loading...";
 
-	// Event Handlers
-	private override function __enterFrame(deltaTime:Float):Void
-	{
-		final now:Float = haxe.Timer.stamp() * 1000;
-		times.push(now);
-		while (times[0] < now - 1000) times.shift();
-		// prevents the overlay from updating every frame, why would you need to anyways @crowplexus
-		if (deltaTimeout < 50) {
-			deltaTimeout += deltaTime;
-			return;
-		}
+        times = [];
+    }
 
-		currentFPS = times.length < FlxG.updateFramerate ? times.length : FlxG.updateFramerate;		
-		updateText();
-		deltaTimeout = 0.0;
-	}
+    var deltaTimeout:Float = 0.0;
 
-	public dynamic function updateText():Void // so people can override it in hscript
-	{
-		text = 
-		'FPS: $currentFPS' + 
-		'\nMemory: ${flixel.util.FlxStringUtil.formatBytes(memoryMegas)}' +
-		os;
+    // Event Handlers
+    private override function __enterFrame(deltaTime:Float):Void
+    {
+        final now:Float = haxe.Timer.stamp() * 1000;
+        times.push(now);
+        while (times[0] < now - 1000) times.shift();
+        // prevents the overlay from updating every frame, why would you need to anyways @crowplexus
+        if (deltaTimeout < 50) {
+            deltaTimeout += deltaTime;
+            return;
+        }
 
-		textColor = 0xFFFFFFFF;
-		if (currentFPS < FlxG.drawFramerate * 0.5)
-			textColor = 0xFFFF0000;
-	}
+        currentFPS = times.length < FlxG.updateFramerate ? times.length : FlxG.updateFramerate;       
+        updateText();
+        deltaTimeout = 0.0;
+    }
 
-	inline function get_memoryMegas():Float
-		return cpp.vm.Gc.memInfo64(cpp.vm.Gc.MEM_INFO_USAGE);
+    public dynamic function updateText():Void // so people can override it in hscript
+    {
+        var currentMemory = memoryMegas;
+        if (currentMemory > memoryPeak) {
+            memoryPeak = currentMemory;
+        }
+        text = 
+        'FPS: $currentFPS' + 
+        '\nMemory: ${flixel.util.FlxStringUtil.formatBytes(memoryMegas)} / ${flixel.util.FlxStringUtil.formatBytes(memoryPeak)}' +
+        os ;
+        if(ClientPrefs.data.exgameversion) text += "\nMintRain Engine 0.2.0\nPsych Engine v1.0";
 
-	public inline function positionFPS(X:Float, Y:Float, ?scale:Float = 1){
-		scaleX = scaleY = #if android (scale > 1 ? scale : 1) #else (scale < 1 ? scale : 1) #end;
-		x = FlxG.game.x + X;
-		y = FlxG.game.y + Y;
-	}
+        textColor = 0xFFFFFFFF;
+        if (currentFPS < FlxG.drawFramerate * 0.5)
+            textColor = 0xFFFF0000;
+    }
 
-	#if cpp
-	#if windows
-	@:functionCode('
-		SYSTEM_INFO osInfo;
+    inline function get_memoryMegas():Float
+        return cpp.vm.Gc.memInfo64(cpp.vm.Gc.MEM_INFO_USAGE);
 
-		GetSystemInfo(&osInfo);
+    inline function get_memoryPeakMegas():Float
+        return memoryPeak;
 
-		switch(osInfo.wProcessorArchitecture)
-		{
-			case 9:
-				return ::String("x86_64");
-			case 5:
-				return ::String("ARM");
-			case 12:
-				return ::String("ARM64");
-			case 6:
-				return ::String("IA-64");
-			case 0:
-				return ::String("x86");
-			default:
-				return ::String("Unknown");
-		}
-	')
-	#elseif (ios || mac)
-	@:functionCode('
-		const NXArchInfo *archInfo = NXGetLocalArchInfo();
-    	return ::String(archInfo == NULL ? "Unknown" : archInfo->name);
-	')
-	#else
-	@:functionCode('
-		struct utsname osInfo{};
-		uname(&osInfo);
-		return ::String(osInfo.machine);
-	')
-	#end
-	@:noCompletion
-	private function getArch():String
-	{
-		return "Unknown";
-	}
-	#end
+    public inline function positionFPS(X:Float, Y:Float, ?scale:Float = 1){
+        scaleX = scaleY = #if android (scale > 1 ? scale : 1) #else (scale < 1 ? scale : 1) #end;
+        x = FlxG.game.x + X;
+        y = FlxG.game.y + Y;
+    }
+
+    #if cpp
+    #if windows
+    @:functionCode('
+        SYSTEM_INFO osInfo;
+
+        GetSystemInfo(&osInfo);
+
+        switch(osInfo.wProcessorArchitecture)
+        {
+            case 9:
+                return ::String("x64");
+            case 5:
+                return ::String("ARMHF");
+            case 12:
+                return ::String("ARM64");
+            case 6:
+                return ::String("IA-64");
+            case 0:
+                return ::String("x86");
+            default:
+                return ::String("Unknown");
+        }
+    ')
+    #elseif (ios || mac)
+    @:functionCode('
+        const NXArchInfo *archInfo = NXGetLocalArchInfo();
+        return ::String(archInfo == NULL ? "Unknown" : archInfo->name);
+    ')
+    #else
+    @:functionCode('
+        struct utsname osInfo{};
+        uname(&osInfo);
+        return ::String(osInfo.machine);
+    ')
+    #end
+    @:noCompletion
+    private function getArch():String
+    {
+        return "Unknown";
+    }
+    #end
 }
